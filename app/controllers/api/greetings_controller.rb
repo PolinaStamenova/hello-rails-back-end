2b8f0f732eb54076f6c9e1a5785fc1a636d05f05
PolinaStamenova <polina.stamenova@gmail.com>
2021-12-01 19:48:59 +0200

    Add safety operator to greetings controller
    When the database is empty its throws an error as `@greeting` is nil.
    To avoid this I use a safety operator.

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -2,7 +2,7 @@ module Api
   class GreetingsController < ::ApplicationController
     def index
       @greeting = Greeting.order(Arel.sql('RANDOM()')).first
-      render json: { greeting: @greeting.message }
+      render json: { greeting: @greeting&.message }
     end
   end
 end
