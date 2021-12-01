297e662ede8c90bdc49d3b4e436be37883a2e7d0
PolinaStamenova <polina.stamenova@gmail.com>
2021-12-01 19:33:21 +0200

    Add root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: 'json' } do
-    get 'greetings', to: 'greetings#index'
+  namespace :api do
+    resources :greetings, only: :index
   end
+
+  root "api/greetings"
 end
